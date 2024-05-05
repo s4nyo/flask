93f55b64e3ea1266bd7c97487c4f6e07c4c77476
s4nyo <118918799+s4nyo@users.noreply.github.com>
2024-05-05 18:14:10 +0300

    Add files via upload

--- flask_master.py
+++ flask_master.py
@@ -3,12 +3,28 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('giro.html')
+    main_data = {
+        'Город': 'Чебоксары',
+        'Район': 'Московский',
+        'Компания': 'Iserv'
+    }
+
+    context = {
+        'Имя': 'Саня',
+        'Возраст': '16',
+        'Профессия': 'безработный'
+    }
+    return render_template('giro.html', main_data = main_data, **context)
 
 @app.route('/contacts/')
 def contacts():
     developer_name = 'Pavel Durov'
     return render_template('contacts.html', name = developer_name)
+
+@app.route('/results/')
+def results():
+    data = ['python', 'js', 'GO', 'C#', 'C++', 'lua', 'React', 'SQL', 'Postgesql']
+    return render_template('results.html', data = data)
 
 if __name__ == "__main__":
     app.run(debug = True)
